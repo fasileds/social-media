6c24a115ef87541c7ecc3ee69845aa42baa9abff
SOli21 <136503849+SOli21@users.noreply.github.com>
2024-01-17 01:01:48 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import PersonIcon from '@mui/icons-material/Person';
+import Home from './pages/home/Home';
+
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Profile/>
+    </div>
+  );
+}
+
+
+export default App;
